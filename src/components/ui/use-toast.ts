2072b0eb6849a3fb050124cd9544621cb93db06c
[dyad] <douglas_mahi@hotmail.com>
2026-02-13 16:52:32 +1100

    [dyad] Renamed use‑toast to .tsx and added a proper Toast wrapper component to resolve the JSX syntax error. - wrote 1 file(s), renamed 1 file(s)

--- src/components/ui/use-toast.ts
+++
@@ -1,5 +0,0 @@
-"use client";
-
-import { Toaster } from "@/components/ui/toaster";
-
-export const Toast = () => <Toaster />;
---
+++ src/components/ui/use-toast.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import React from "react";
+import { Toaster } from "@/components/ui/toast";
+
+export const Toast = () => <Toaster />;
